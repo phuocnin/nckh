9b2af8c1b4dafa5cc9d9bbd934a68d1e7654273c
thanhthanhnguyen82 <thanhbachthu@gmail.com>
2023-11-21 12:09:31 +0700

    okela

--- src/routes/view.router.js
+++ src/routes/view.router.js
@@ -17,4 +17,33 @@ Router.get("/user", async (req, res) => {
   // const topics = await topicModel.findById(req.params.id);
   res.status(200).render("view_user");
 });
+Router.get("/project", async (req, res) => {
+  // const topics = await topicModel.findById(req.params.id);
+  res.status(200).render("project_list");
+});
+Router.get("/manageprogress", async (req, res) => {
+  // const topics = await topicModel.findById(req.params.id);
+  res.status(200).render("manageprogress");
+});
+Router.get("/managetask", async (req, res) => {
+  // const topics = await topicModel.findById(req.params.id);
+  res.status(200).render("managetask");
+});
+Router.get("/manageusers", async (req, res) => {
+  // const topics = await topicModel.findById(req.params.id);
+  res.status(200).render("manageusers");
+});
+Router.get("/newproject", async (req, res) => {
+  // const topics = await topicModel.findById(req.params.id);
+  res.status(200).render("newproject");
+});
+Router.get("/report", async (req, res) => {
+  // const topics = await topicModel.findById(req.params.id);
+  res.status(200).render("report");
+});
+Router.get("/newuser", async (req, res) => {
+  // const topics = await topicModel.findById(req.params.id);
+  res.status(200).render("new_user");
+});
+
 module.exports = Router;
