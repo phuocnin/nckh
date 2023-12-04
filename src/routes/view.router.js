93f95a49b5bff6745b2d3b5876f1b9fde8bb06ec
Hoang Gia <nhg1ew@gmail.com>
2023-12-04 20:24:31 +0700

    first commit

--- src/routes/view.router.js
+++ src/routes/view.router.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require("../controllers/auth.controller");
 const Router = express.Router();
 
 Router.get("/login", (req, res) => {
-  res.status(200).render("login");
+    res.status(200).render("login");
 });
 
 //home
@@ -23,41 +23,45 @@ Router.get("/me", authMiddleware, userController.getMe, userController.getUser);
 // done href
 Router.get("/topics", topicController.getTopics);
 
-Router.get("/manageusers", async (req, res) => {
-  // const topics = await topicModel.findById(req.params.id);
-  res.status(200).render("manageusers");
+Router.get("/manageusers", async(req, res) => {
+    // const topics = await topicModel.findById(req.params.id);
+    res.status(200).render("manageusers");
 });
 
 // thêm đề tài
-Router.get("/new_topic", async (req, res) => {
-  res.status(200).render("newproject");
+Router.get("/new_topic", async(req, res) => {
+    res.status(200).render("newproject");
+});
+// xem đề tài
+Router.get("/viewtopic", async(req, res) => {
+    res.status(200).render("view_topic");
 });
 
-Router.get("/newuser", async (req, res) => {
-  res.status(200).render("new_user");
+Router.get("/newuser", async(req, res) => {
+    res.status(200).render("new_user");
 });
-Router.get("/userlist", async (req, res) => {
-  const users = await userModel.find();
-  res.status(200).render("userlist", { users });
+Router.get("/userlist", async(req, res) => {
+    const users = await userModel.find();
+    res.status(200).render("userlist", { users });
 });
-Router.get("/report", async (req, res) => {
-  res.status(200).render("report");
+Router.get("/report", async(req, res) => {
+    res.status(200).render("report");
 });
-Router.get("/manageprogress", async (req, res) => {
-  res.status(200).render("manageprogress");
+Router.get("/manageprogress", async(req, res) => {
+    res.status(200).render("manageprogress");
 });
-Router.get("/managetask", async (req, res) => {
-  res.status(200).render("managetask");
+Router.get("/managetask", async(req, res) => {
+    res.status(200).render("managetask");
 });
-Router.get("/notify", async (req, res) => {
-  res.status(200).render("notify");
+Router.get("/notify", async(req, res) => {
+    res.status(200).render("notify");
 });
 Router.get("/notify/:id", viewController.viewNotify);
 Router.get("/editnotify/:id", viewController.editNotify);
-Router.get("/fixuser/:id", async (req, res) => {
-  res.status(200).render("edituser");
+Router.get("/fixuser/:id", async(req, res) => {
+    res.status(200).render("edituser");
 });
-Router.get("/changepass", authMiddleware, async (req, res) => {
-  res.status(200).render("changepass");
+Router.get("/changepass", authMiddleware, async(req, res) => {
+    res.status(200).render("changepass");
 });
 module.exports = Router;
