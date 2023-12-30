0baf5b9b7dd99b153502083df4e7a434a8df9acb
thanhthanhnguyen82 <thanhbachthu@gmail.com>
2023-12-30 16:39:32 +0700

    chat

--- src/public/mjs/chat.js
+++ src/public/mjs/chat.js
@@ -1,27 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Event listener for form submission
   const messageForm = document.getElementById("messageForm");
+
   if (messageForm) {
     messageForm.addEventListener("submit", function (e) {
       e.preventDefault();
-      const messageInput = document.querySelector('input[name="message"]');
-      const message = messageInput.value.trim();
 
-      if (message) {
-        sendMessage(message);
-        addMessageToChatWindow('Bạn', message, true);
+      const formData = new FormData(this);
+      const message = formData.get('message').trim();
+
+      if (message || formData.get('file')) {
+        sendMessage(formData);
+        addMessageToChatWindow('Bạn', message || 'File đã gửi', true);
       }
-
-      messageInput.value = "";
     });
   }
 
+  // Event listener for search button
   const searchButton = document.querySelector(".input-group-append button");
+
   if (searchButton) {
     searchButton.addEventListener("click", function () {
       const searchInput = document.querySelector('.input-group input[type="text"]');
       const searchTerm = searchInput.value.toLowerCase();
 
       const userList = document.getElementById("userList");
+
       if (userList) {
         const users = userList.querySelectorAll(".list-group-item");
         users.forEach((user) => {
@@ -33,70 +37,69 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-
-function sendMessage(message) {
-  axios.post('/api/v1/messages', { content: message })
-    .then(function (response) {
-       console.log('Tin nhắn đã được gửi');
-     })
-     .catch(function (error) {
-       console.error('Lỗi khi gửi tin nhắn:', error);
-     });
+// Function to send a message using axios
+function sendMessage(formData) {
+  axios.post('/api/v1/messages', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+  .then(function (response) {
+    console.log('Tin nhắn đã được gửi');
+  })
+  .catch(function (error) {
+    console.error('Lỗi khi gửi tin nhắn:', error);
+  });
 }
 
-
-function addMessageToChatWindow(username, message, isSender) {
+// Function to add a message to the chat window
+function addMessageToChatWindow(username, message, isSender, fileUrl) {
   const chatWindow = document.getElementById("chatWindow");
   const messageElement = document.createElement("div");
   messageElement.classList.add('message', isSender ? 'sender' : 'receiver');
-  messageElement.innerHTML = `<strong>${username}:</strong> ${message}`;
+  
+  let messageContent = `<strong>${username}:</strong> `;
+
+  if (fileUrl) {
+    if (fileUrl.match(/\.(jpeg|jpg|gif|png)$/i)) {
+      // If the file is an image, display it as an image tag
+      messageContent += `<img src="${fileUrl}" alt="Sent Image" style="max-width:200px;">`;
+    } else {
+      // If the file is not an image, provide a download link
+      messageContent += `<a href="${fileUrl}" target="_blank" download>Download File</a>`;
+    }
+  } else {
+    // If there is no file, display the text message
+    messageContent += message;
+  }
+
+  messageElement.innerHTML = messageContent;
   chatWindow.appendChild(messageElement);
-  chatWindow.scrollTop = chatWindow.scrollHeight; 
+  chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
+// Function to load a conversation using axios
 function loadConversation(conversationId, index) {
   axios.get(`/api/v1/conversations/${conversationId}`)
     .then(function (response) {
-       console.log('Tin nhắn đã được gửi');
-     })
-     .catch(function (error) {
-       console.error('Lỗi khi gửi tin nhắn:', error);
-     });
-}
-
-
-function addMessageToChatWindow(username, message) {
-  const chatWindow = document.getElementById("chatWindow");
-  const messageElement = document.createElement("div");
-  messageElement.classList.add('message');
-  messageElement.innerHTML = `<strong>${username}:</strong> ${message}`;
-  chatWindow.appendChild(messageElement);
-  chatWindow.scrollTop = chatWindow.scrollHeight; 
-}
-
-// Hàm để tải cuộc hội thoại
-function loadConversation(conversationId, index) {
-  
-   axios.get(`/api/v1/conversations/${conversationId}`)
-   .then(function (response) {
       const { data } = response;
       const chatName = document.getElementById("chatName");
       const chatWindow = document.getElementById("chatWindow");
 
-       if (index == 3) {
-         chatName.textContent = data.data.name;
-       } else {
-         chatName.textContent = data.data.participants[index].name;
-       }
+      if (index == 3) {
+        chatName.textContent = data.data.name;
+      } else {
+        chatName.textContent = data.data.participants[index].name;
+      }
 
-       chatWindow.innerHTML = "";
+      chatWindow.innerHTML = "";
       const messages = data.data.messages;
 
       messages.forEach((message) => {
-         addMessageToChatWindow(message.username, message.content);
-       });
-     })
-     .catch(function (error) {
-       console.error('Lỗi khi tải cuộc hội thoại:', error);
-     });
+        addMessageToChatWindow(message.username, message.content);
+      });
+    })
+    .catch(function (error) {
+      console.error('Lỗi khi tải cuộc hội thoại:', error);
+    });
 }
